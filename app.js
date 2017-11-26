f69507aca5ce3be25218e7c113015533c32a2204
huweiying <huweiying0228@163.com>
2017-11-26 15:57:36 +0800

    add ejs msg

--- app.js
+++ app.js
@@ -3,9 +3,10 @@
  */
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
-const mysql = require('mysql');
-const bcrypt = require('bcryptjs');
+const bodyParser = require('body-parser');//获取输入数据
+const mysql = require('mysql');//数据库模块
+const bcrypt = require('bcryptjs');//加密模块
+const ejs = require('ejs');//模板字符串
 let app = express();
 let pool = mysql.createPool({
     database : 'blog',
@@ -13,17 +14,21 @@ let pool = mysql.createPool({
 })
 // 使用中间件
 app.use(bodyParser.urlencoded({extended:true}));
+app.engine('.html',ejs.__express);//配置模板引擎
+app.set('view engine','html');
 //引入默认页面
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'views/default.html'));
 });
 //获取注册页
 app.get('/register',(req,res)=>{
-    res.sendFile(path.join(__dirname,'views/register.html'));
+    // res.sendFile(path.join(__dirname,'views/register.html'));
+    res.render('register',{msg:null});
 })
 //获取登录页
 app.get('/login',(req,res)=>{
-    res.sendFile(path.join(__dirname,'views/login.html'));
+    // res.sendFile(path.join(__dirname,'views/login.html'));
+    res.render('login',{msg:null});
 });
 //提交注册信息
 app.post('/register',(req,res)=>{
@@ -37,17 +42,20 @@ app.post('/register',(req,res)=>{
         conn.query('SELECT * FROM user WHERE username = ?',[username],(err,result,fields)=>{
             if (err) throw err;
             if(result.length === 1){
-                res.sendFile(path.join(__dirname,'views/register.html'));
+                // res.sendFile(path.join(__dirname,'views/register.html'));
+                res.render('register',{msg:'username exists'});
             }else{
                 console.log(result.length);
                 conn.query('INSERT INTO user VALUE(null,?,?)',[username,encryptedPassword],(err,result,fields)=>{
                     if (err) throw err;
                     if(result.affectedRows === 1){
                         //注册成功
-                        res.sendFile(path.join(__dirname,'views/login.html'));
+                        // res.sendFile(path.join(__dirname,'views/login.html'));
+                        res.render('login',{msg:null});
                     }else{
                         //注册失败
-                        res.sendFile(path.join(__dirname,'views/register.html'));
+                        // res.sendFile(path.join(__dirname,'views/register.html'));
+                        res.render('register',{msg:'注册失败'});
                     }
                 });
             }
@@ -70,8 +78,12 @@ app.post('/login',(req,res)=>{
                 if (bcrypt.compareSync(password, encryptedPassword)) {
                     res.sendFile(path.join(__dirname, 'views/default.html'));
                 } else {
-                    res.sendFile(path.join(__dirname, 'views/login.html'));
+                    // res.sendFile(path.join(__dirname, 'views/login.html'));
+                    res.render('login',{msg:'用户名或密码错误'});
                 }
+            } else {
+                // res.sendFile(path.join(__dirname, 'views/login.html'));
+                res.render('login',{msg:'用户名或密码错误'});
             }
 
         })
