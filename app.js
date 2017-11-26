0df282a930294dd4026bcb622d72217db69a3db3
huweiying <huweiying0228@163.com>
2017-11-26 14:31:39 +0800

    update register

--- app.js
+++ app.js
@@ -29,21 +29,30 @@ app.get('/login',(req,res)=>{
 app.post('/register',(req,res)=>{
     let username = req.body.username;
     let password = req.body.password;
-    console.log(username,password);
     pool.getConnection((err,conn)=>{
         if (err) throw err;
-        conn.query('INSERT INTO user VALUE(null,?,?)',[username,password],(err,result,fields)=>{
+        //如果用户名存在，则不插入
+        conn.query('SELECT * FROM user WHERE username = ?',[username],(err,result,fields)=>{
             if (err) throw err;
-            if(result.affectedRows === 1){
-                //注册成功
-                res.sendFile(path.join(__dirname,'views/login.html'));
-            }else{
-                //注册失败
+            if(result.length === 1){
                 res.sendFile(path.join(__dirname,'views/register.html'));
+            }else{
+                console.log(result.length);
+                conn.query('INSERT INTO user VALUE(null,?,?)',[username,password],(err,result,fields)=>{
+                    if (err) throw err;
+                    if(result.affectedRows === 1){
+                        //注册成功
+                        res.sendFile(path.join(__dirname,'views/login.html'));
+                    }else{
+                        //注册失败
+                        res.sendFile(path.join(__dirname,'views/register.html'));
+                    }
+                });
             }
         });
         conn.release();
     })
+
 })
 //提交登录信息
 app.post('/login',(req,res)=>{
